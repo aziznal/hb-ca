07e2bbb8f6286d6610e146efd659438c753462b1
aziznal <aziznal.dev@gmail.com>
2020-11-03 21:20:47 +0300

    Refactor - Move old scraping script from main to functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,10 @@
+from hepsiBot import hepsiBot, categories
+from selenium.webdriver.firefox.options import Options
+from unidecode import unidecode
+
+from functions import save_as_csv
+
+
 
 def save_as_csv(data, col_names, filename, overwrite=False):
     """
@@ -15,4 +22,28 @@ def save_as_csv(data, col_names, filename, overwrite=False):
                 file.write(f"{unidecode(str(entry[key]))},")
 
             file.write('\n')
+
+
+def scrape_category():
+    """
+    This method is here as a rough guide to how I scraped the notebook and phone categories
+    """
+    bot = hepsiBot(url=categories['phone'], options=custom_options)
+
+    bot.wait()
+
+    thumbnails = []
+
+    for _ in range(bot.total_page_num):
+        thumbnails += bot.get_all_products()
+        try:
+            bot.goto_next_page()
+        
+        except Exception as e:
+            break;
+
+    formatted_thumbnails = [thumbnail.clean_soup for thumbnail in thumbnails]
+    column_names = ["instock", "title", "url", "comment_amount", "rating", "price"]
+
+    save_as_csv(data=formatted_thumbnails, col_names=column_names, filename="phone_thumbnails.csv", overwrite=True)
 
--- main.py
+++ main.py
@@ -15,25 +15,7 @@ custom_options.set_preference("permissions.default.image", 2)
 
 
 def run_program():
-    bot = hepsiBot(url=categories['phone'], options=custom_options)
-
-    bot.wait()
-
-    thumbnails = []
-
-    for _ in range(bot.total_page_num):
-        thumbnails += bot.get_all_products()
-        try:
-            bot.goto_next_page()
-        
-        except Exception as e:
-            break;
-
-    formatted_thumbnails = [thumbnail.clean_soup for thumbnail in thumbnails]
-    column_names = ["instock", "title", "url", "comment_amount", "rating", "price"]
-
-    save_as_csv(data=formatted_thumbnails, col_names=column_names, filename="phone_thumbnails.csv", overwrite=True)
-
+    pass
 
 if __name__ == "__main__":
     run_program()
